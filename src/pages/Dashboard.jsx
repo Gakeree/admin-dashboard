5167fb1c9c4cd0fc581a347891e037ca3e62b123
Gakeree <kenedygakere@gmail.com>
2023-09-23 20:41:29 +0300

    progress

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState } from 'react'
+import Header from '../components/Header'
+import Sidebar from '../components/sidebar'
+import Home from '../components/Home '
+
+const Dashboard = () => {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  } 
+  
+  return (
+    <div className='grid-container'> 
+        <Header OpenSidebar={OpenSidebar} />
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
+  )
+}
+
+export default Dashboard
